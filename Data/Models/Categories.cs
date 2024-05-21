c9a4c2425d32a2faa6e15e13a8ec283d6638dbd3
nightSanches <galesavich@gmail.com>
2024-05-21 20:31:22 +0500

    Разработанные модели данных. Савичев

--- Data/Models/Categories.cs
+++ Data/Models/Categories.cs
@@ -8,6 +8,7 @@ namespace pr37savichev.Data.Models
     public class Categories
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Items> Items { get; set; }
--- Data/Models/Items.cs
+++ Data/Models/Items.cs
@@ -8,6 +8,7 @@ namespace pr37savichev.Data.Models
     public class Items
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string Img { get; set; }
